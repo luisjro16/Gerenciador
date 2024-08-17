d2c1d8f42f92da252864f68394c4294e5b5dae16
luisjro16 <luis_jeronimo111@outlook.com>
2024-08-17 15:47:28 -0300

    Apagando models

--- app/models/authenticated.rb
+++
@@ -1,6 +0,0 @@
-class Authenticated < ApplicationRecord
-  # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-end
