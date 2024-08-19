6aa18e0ec7f9a19aeaa8d5db5381395b1f770b7a
luisjro16 <luis_jeronimo111@outlook.com>
2024-08-19 17:27:34 -0300

    Correção de posição de criação de user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!  
+  before_action :authenticate_user! 
 
   def show
     @user = current_user  
   end
 
   def edit
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Usuário criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :access_level, :department_ids, department_ids: [])
   end
 end
